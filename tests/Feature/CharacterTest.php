b23740a16d139c7f643ad98d6464f33039c61ef6
Talita-8 <talita.silva@pravaler.com>
2021-07-29 17:18:49 -0300

    feat: Testes unitários

---
+++ tests/Feature/CharacterTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CharacterTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testCreateCharacterWithAllParams()
+    {
+        $response = $this->postJson('api/characters', 
+        ['name' => 'Rick',
+         'status' => 'Alive', 
+         'species' => 'Human', 
+         'type' => 'None', 
+         'gender' => 'Male', 
+         'image' => 'https://raw.githubusercontent.com/Laboratoria/rick-and-morty-images/master/images/1.jpeg'
+        ]);
+
+        $response->assertStatus(201);
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testCreateCharacterWithoutAllParams()
+    {
+        $response = $this->postJson('api/characters', 
+        ['name' => 'Rick',
+         'status' => '', 
+         'species' => 'Human', 
+         'type' => 'None', 
+         'gender' => 'Male', 
+         'image' => 'https://raw.githubusercontent.com/Laboratoria/rick-and-morty-images/master/images/1.jpeg'
+        ]);
+
+        $response->assertStatus(500);
+    }
+}
